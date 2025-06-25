3392a7324e07498858124a2d3033484630b13081
Default <hedgehog2408@gmail.com>
2025-06-25 17:13:44 +0300

    Added ApplicationDbContext and SeedData for first filling database

---
+++ DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DAL.Data
+{
+	internal class ApplicationDbContext : IdentityDbContext<Users>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options) { }
+
+		public override DbSet<Users> Users { get; set; }
+		public DbSet<Courses> Courses { get; set; }
+		public DbSet<Enrollments> Enrollments { get; set; }
+		public DbSet<Lessons> Lessons { get; set; }
+		public DbSet<Reviews> Reviews { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+			var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+				v => v.ToUniversalTime(),
+				v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+			foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+			{
+				var properties = entityType.GetProperties()
+					.Where(p => p.ClrType == typeof(DateTime));
+
+				foreach (var property in properties)
+				{
+					property.SetValueConverter(dateTimeConverter);
+				}
+			}
+		}
+	}
+}
---
+++ DAL/Data/SeedData.cs
@@ -0,0 +1,125 @@
+﻿using Bogus;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+	internal class SeedData
+	{
+		public static async Task SeedAsync(
+			ApplicationDbContext context,
+			UserManager<Users> userManager,
+			RoleManager<IdentityRole> roleManager)
+		{
+			await context.Database.MigrateAsync();
+
+			// 1. Ролі
+			var roles = new[] { "Admin", "User" };
+			foreach (var role in roles)
+			{
+				if (!await roleManager.RoleExistsAsync(role))
+					await roleManager.CreateAsync(new IdentityRole(role));
+			}
+
+			// 2. Користувачі
+			if (!context.Users.Any())
+			{
+				var usersFaker = new Faker<Users>()
+					.RuleFor(u => u.UserName, f => f.Internet.UserName())
+					.RuleFor(u => u.Email, f => f.Internet.Email())
+					.RuleFor(u => u.CreatedAt, f => f.Date.Past(1))
+					.RuleFor(u => u.EmailConfirmed, _ => true);
+
+				var users = usersFaker.Generate(10);
+				const string password = "1234567890";
+
+				foreach (var user in users)
+				{
+					var result = await userManager.CreateAsync(user, password);
+					if (result.Succeeded)
+						await userManager.AddToRoleAsync(user, "User");
+				}
+
+				var admin = new Users
+				{
+					UserName = "admin",
+					Email = "admin@itcourses.com",
+					EmailConfirmed = true,
+					CreatedAt = DateTime.UtcNow
+				};
+
+				if ((await userManager.CreateAsync(admin, password)).Succeeded)
+					await userManager.AddToRoleAsync(admin, "Admin");
+			}
+
+			// 3. Курси
+			if (!context.Courses.Any())
+			{
+				var instructors = await context.Users.ToListAsync();
+
+				var courseFaker = new Faker<Courses>()
+					.RuleFor(c => c.title, f => f.Company.CatchPhrase())
+					.RuleFor(c => c.description, f => f.Lorem.Paragraph())
+					.RuleFor(c => c.instructor_id, f => f.PickRandom(instructors).Id)
+					.RuleFor(c => c.created_at, f => f.Date.Past(1));
+
+				var courses = courseFaker.Generate(5);
+				await context.Courses.AddRangeAsync(courses);
+				await context.SaveChangesAsync();
+
+				// 4. Уроки
+				foreach (var course in courses)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						var lesson = new Lessons
+						{
+							course_id = course.Id,
+							title = new Faker().Lorem.Sentence(),
+							content = new Faker().Lorem.Paragraphs(2),
+							lesson_order = i + 1
+						};
+						await context.Lessons.AddAsync(lesson);
+					}
+				}
+				await context.SaveChangesAsync();
+			}
+
+			// Попередньо завантажені дані
+			var allUsers = await context.Users.ToListAsync();
+			var allCourses = await context.Courses.ToListAsync();
+
+			// 5. Enrollments
+			if (!context.Enrollments.Any())
+			{
+				var enrollmentsFaker = new Faker<Enrollments>()
+					.RuleFor(e => e.user_id, f => f.PickRandom(allUsers).Id)
+					.RuleFor(e => e.course_id, f => f.PickRandom(allCourses).Id)
+					.RuleFor(e => e.enrolled_at, f => f.Date.Past(1));
+
+				var enrollments = enrollmentsFaker.Generate(10);
+				await context.Enrollments.AddRangeAsync(enrollments);
+				await context.SaveChangesAsync();
+			}
+
+			// 6. Reviews
+			if (!context.Reviews.Any())
+			{
+				var reviewsFaker = new Faker<Reviews>()
+					.RuleFor(r => r.course_id, f => f.PickRandom(allCourses).Id)
+					.RuleFor(r => r.user_id, f => f.PickRandom(allUsers).Id)
+					.RuleFor(r => r.rating, f => f.Random.Int(1, 5))
+					.RuleFor(r => r.comment, f => f.Lorem.Sentence())
+					.RuleFor(r => r.created_at, f => f.Date.Recent(30));
+
+				var reviews = reviewsFaker.Generate(10);
+				await context.Reviews.AddRangeAsync(reviews);
+				await context.SaveChangesAsync();
+			}
+		}
+	}
+}
