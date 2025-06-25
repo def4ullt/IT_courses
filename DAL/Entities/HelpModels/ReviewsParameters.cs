d7b61996134521095fcc115d9c5543e6348c3e5b
Default <hedgehog2408@gmail.com>
2025-06-25 18:38:59 +0300

    Add configuration classes for Enrollments, Lessons, Reviews

---
+++ DAL/Entities/HelpModels/ReviewsParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.HelpModels
+{
+	internal class ReviewsParameters : QueryStringParameters
+	{
+		public string? SearchTerm { get; set; }
+		public int? CourseId { get; set; }
+		public int? UserId { get; set; }
+		public DateTime? CreatedAfter { get; set; }
+		public DateTime? CreatedBefore { get; set; }
+		public string? SortBy { get; set; }
+		public bool SortDescending { get; set; } = false;
+	}
+}
---
+++ DAL/Entities/HelpModels/UsersParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.HelpModels
+{
+	internal class UsersParameters : QueryStringParameters
+	{
+		public string? SearchTerm { get; set; }
+		public DateTime? CreatedAfter { get; set; }
+		public DateTime? CreatedBefore { get; set; }
+		public string? SortBy { get; set; }
+		public bool SortDescending { get; set; } = false;
+	}
+}
