2986bfc182c8aa479256633d7ba71a7c8c09a1b8
Default <hedgehog2408@gmail.com>
2025-06-26 12:14:48 +0300

    Added Unit Of work

---
+++ DAL/UOW/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.UOW
+{
+	internal interface IUnitOfWork : IDisposable
+	{
+		IEnrollmentsRepository Enrollments { get; }
+		IUsersRepository Users { get; }
+		IReviewsRepository Reviews { get; }
+		ILessonsRepository Lessons { get; }
+		ICoursesRepository Courses { get; }
+	
+		Task<int> SaveChangesAsync();
+	}
+}
---
+++ DAL/UOW/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories.Interfaces;
+using DAL.Data;
+namespace DAL.UOW
+{
+	internal class UnitOfWork : IUnitOfWork
+	{
+		private readonly ApplicationDbContext _context;
+
+		public IUsersRepository Users { get; }
+		public ICoursesRepository Courses { get; }
+		public ILessonsRepository Lessons { get; }
+		public IReviewsRepository Reviews { get; }
+		public IEnrollmentsRepository Enrollments { get; }
+
+		public UnitOfWork(ApplicationDbContext context,
+			IUsersRepository usersRepository,
+			ICoursesRepository coursesRepository,
+			ILessonsRepository lessonsRepository,
+			IReviewsRepository reviewsRepository,
+			IEnrollmentsRepository enrollmentsRepository)
+		{
+			_context = context;
+			Users = usersRepository;
+			Courses = coursesRepository;
+			Lessons = lessonsRepository;
+			Reviews = reviewsRepository;
+			Enrollments = enrollmentsRepository;
+		}
+
+		public async Task<int> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync();
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+	}
+}
