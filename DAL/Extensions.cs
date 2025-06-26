05c628b9fcee004415f2cba8a6d881ba15d84ecc
Default <hedgehog2408@gmail.com>
2025-06-26 12:31:26 +0300

    Update DAL project configuration and add service extensions
    Removed files from Entities\NewFolder in DAL.csproj and updated project to target .NET 8.0 with implicit usings and nullable reference types enabled.
    
    Expanded Extensions.cs with a new static class to register repository interfaces and implementations, including unit of work and sort helpers for various entity types, enhancing the Data Access Layer (DAL) service configuration.

---
+++ DAL/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using DAL.Helpers;
+using DAL.Repositories;
+using DAL.Repositories.Interfaces;
+using DAL.UOW;
+using DAL.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+	internal static class Extensions
+	{
+		public static IServiceCollection AddDALServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddScoped<IUsersRepository, UsersRepository>();
+			services.AddScoped<ICoursesRepository, CoursesRepository>();
+			services.AddScoped<ILessonsRepository, LessonsRepository>();
+			services.AddScoped<IReviewsRepository, ReviewsRepository>();
+			services.AddScoped<IEnrollmentsRepository, EnrollmentsRepository>();
+
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+			
+			services.AddScoped<ISortHelper<Courses>, SortHelper<Courses>>();
+			services.AddScoped<ISortHelper<Lessons>, SortHelper<Lessons>>();
+			services.AddScoped<ISortHelper<Users>, SortHelper<Users>>();
+			services.AddScoped<ISortHelper<Reviews>, SortHelper<Reviews>>();
+			services.AddScoped<ISortHelper<Enrollments>, SortHelper<Enrollments>>();
+
+			return services;
+		}
+	}
+}
